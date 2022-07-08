3f93bbb57fde6ec44d6d2d8daa35f6c480f2cb72
SufurElite <13058966+SufurElite@users.noreply.github.com>
2022-07-08 07:44:01 +0100

    Added June's Data and found a great resource on multilat

---
+++ Code/plot.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+from utils import loadData, loadNodes
+
+def plotRegular():
+    
+    pass
+
+def main():
+    return None
+
+if __name__=="__main__":
+    parser.add_argument('--data', dest='data', type=str, help='The data selection you want to visualise')
+    parser.add_argument('--multiLat', dest='data', type=bool, help='Whether you want to see the multilat  given the values')
+    parser.add_argument('--model', dest='data', type=str, help='If you want to see the reduced error predicted estimate, which model')
+    
+
+    args = parser.parse_args()
+    main()

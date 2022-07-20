08489f6fdf93cea37ab2bee44ea13c72d1d364c2
SufurElite <13058966+SufurElite@users.noreply.github.com>
2022-07-20 07:58:42 +0100

    Added validation test split

--- Code/model.py
+++ Code/model.py
@@ -12,10 +12,13 @@ from sklearn.model_selection import train_test_split
 def rmseModel():
     """ Root mean squared error XGBoost trained on the june data"""
     X, y = loadModelData(month="June",threshold=-86, verbose=False)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=101)
+
+    X_train, X_remaining, y_train, y_remaining = train_test_split(X, y, train_size=0.8, random_state=101)
+    X_valid, X_test, y_valid, y_test = train_test_split(X_remaining,y_remaining, test_size=0.5)
+
     # Train a regressor on it
-    reg = xgb.XGBRegressor(tree_method="hist", n_estimators=64)
-    reg.fit(X_train, y_train, eval_set=[(X_test, y_test)])
+    reg = xgb.XGBRegressor(tree_method="hist", n_estimators=32)
+    reg.fit(X_train, y_train, eval_set=[(X_valid, y_valid)])
     
     yPred = reg.predict(X)
     avgError = [0,0]
@@ -28,17 +31,17 @@ def rmseModel():
         avgError[1]+=1
 
     yTestPred = reg.predict(X_test)
-    testError = [0,0]
-    
+    allErrors = []
     for idx in range(len(yTestPred)):
         gt = np.array([np.float64(X_test[idx][0]+y_test[idx][0]), np.float64(X_test[idx][1]+y_test[idx][1])])
         predicted = np.array([np.float64(yTestPred[idx][0]+X_test[idx][0]),np.float64(yTestPred[idx][1]+X_test[idx][1])])
         dist = np.linalg.norm(predicted-gt)
-        testError[0]+=dist
-        testError[1]+=1
+        allErrors.append(dist)
 
+    print(len(X_valid),len(X_test),len(X_train),len(X))
     print("The total average error is {}m".format(avgError[0]/avgError[1]))
-    print("The test average error is {}m".format(testError[0]/testError[1]))
+    print("The test average error is {}m with a maximum of {}".format(sum(allErrors)/len(allErrors), max(allErrors)))
+    print(allErrors)
 
 if __name__ == "__main__":
     rmseModel()

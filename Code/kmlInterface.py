ca63edafd768b4b2e18922e9be5f0e05c849533e
SufurElite <13058966+SufurElite@users.noreply.github.com>
2022-08-31 18:35:20 +0100

    kml init

---
+++ Code/kmlInterface.py
@@ -0,0 +1,10 @@
+"""
+    This file will deal with parsing the values from the Habitat Map in the data to better deal with and predict habitat covariates better.
+    This will make use of the shapely library. 
+"""
+
+class habitatMap():
+    HABITAT_TYPES = ["bamboo","forest","high_paramo","open_paramo"]
+    def __init__(self):
+        pass
+    pass
